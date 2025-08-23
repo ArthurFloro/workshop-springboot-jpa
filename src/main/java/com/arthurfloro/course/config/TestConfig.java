0a8885abc7ea5ca298892bdb4991a1b45a51cae4
ArthurFloro <arthur.floro2006@gmail.com>
2025-08-22 21:55:56 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/arthurfloro/course/config/TestConfig.java
+++ src/main/java/com/arthurfloro/course/config/TestConfig.java
@@ -3,20 +3,14 @@ package com.arthurfloro.course.config;
 import java.time.Instant;
 import java.util.Arrays;
 
+import com.arthurfloro.course.entities.*;
+import com.arthurfloro.course.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.arthurfloro.course.entities.Category;
-import com.arthurfloro.course.entities.Order;
-import com.arthurfloro.course.entities.Product;
-import com.arthurfloro.course.entities.User;
 import com.arthurfloro.course.enums.OrderStatus;
-import com.arthurfloro.course.repositories.CategoryRepository;
-import com.arthurfloro.course.repositories.OrderRepository;
-import com.arthurfloro.course.repositories.ProductRepository;
-import com.arthurfloro.course.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
@@ -33,6 +27,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -68,6 +65,13 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 
 }
---
+++ src/main/java/com/arthurfloro/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.arthurfloro.course.repositories;
+
+import com.arthurfloro.course.entities.Category;
+import com.arthurfloro.course.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
