c35d98e4e7f7b47d1abd570415768159159623d3
ArthurFloro <arthur.floro2006@gmail.com>
2025-08-23 21:23:39 -0300

    SubTotal & Total methods

--- src/main/java/com/arthurfloro/course/entities/Order.java
+++ src/main/java/com/arthurfloro/course/entities/Order.java
@@ -93,6 +93,14 @@ public class Order implements Serializable {
 		return items;
 	}
 
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem item : items) {
+			sum = sum + item.getSubTotal();
+		}
+		return sum;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
