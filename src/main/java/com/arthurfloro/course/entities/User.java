f1625480aa354bb38f0c4f1a9aa192201bec9c38
ArthurFloro <arthur.floro2006@gmail.com>
2025-08-24 15:32:50 -0300

    User update

--- src/main/java/com/arthurfloro/course/entities/User.java
+++ src/main/java/com/arthurfloro/course/entities/User.java
@@ -1,17 +1,12 @@
 package com.arthurfloro.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 
 
 @Entity
@@ -21,7 +16,7 @@ public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -43,20 +38,19 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-	
-	public Long getId() {	
+	public Long getId() {
 		return id;
 	}
 
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
-	public void SetName(String name) {
+
+	public void setName(String name) {
 		this.name = name;
 	}
 
