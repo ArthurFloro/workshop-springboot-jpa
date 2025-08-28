21c4564d50fe05ad60c8d07badcffd56ce5db92a
ArthurFloro <arthur.floro2006@gmail.com>
2025-08-27 22:14:00 -0300

    Exception handling - update

--- src/main/java/com/arthurfloro/course/services/UserService.java
+++ src/main/java/com/arthurfloro/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.arthurfloro.course.entities.User;
 import com.arthurfloro.course.repositories.UserRepository;
 import com.arthurfloro.course.services.exceptions.DataBaseException;
 import com.arthurfloro.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +44,13 @@ public class UserService {
 
 	@Transactional
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
